1740dccf914d84917df4794104ea07d6448b0c10
EEDaryl <ejohdaryl@gmail.com>
2024-03-06 23:58:45 +0100

    target type = ip

---
+++ 30-EKS-Ingress-TargetType-IP/01-ekscluster-terraform/c8-01-iam-admin-user.tf
@@ -0,0 +1,29 @@
+# Resource: AWS IAM User - Basic User (No AWS Console Access)
+resource "aws_iam_user" "basic_user" {
+  name = "${local.name}-eksadmin2"
+  path = "/"
+  force_destroy = true
+  tags = local.common_tags
+}
+
+
+# Resource: AWS IAM User Policy - EKS Full Access
+resource "aws_iam_user_policy" "basic_user_eks_policy" {
+  name   = "${local.name}-eks-full-access-policy"
+  user   = aws_iam_user.basic_user.name
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "iam:ListRoles",
+          "eks:*",
+          "ssm:GetParameter"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+        #Resource = "${aws_eks_cluster.eks_cluster.arn}"
+      },
+    ]
+  })
+}
